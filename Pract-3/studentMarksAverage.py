25b46ce902d6e35dc2efef366a257fc57d64ec0c
Uday Valera <126170806+udayvalera@users.noreply.github.com>
2024-08-14 09:21:01 +0000

    Update Pract-2 reports and add new student grade processing scripts
    - Modified Pract-2/passengers.csv
    - Modified Pract-2/report_1.txt
    - Modified Pract-2/report_2.txt
    - Deleted Pract-3/new.py
    - Added Pract-3/studentMarksAverage.py
    - Added Pract-3/student_average_grades.csv
    - Added Pract-3/student_grades.csv

---
+++ Pract-3/studentMarksAverage.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+student_grades = pd.read_csv('student_grades.csv')
+
+# Calculate the average of the student marks
+student_avgGrades = pd.DataFrame(columns=['Roll No', 'Student Name', 'Average Marks'])
+student_avgGrades['Roll No'] = student_grades['Roll No']
+student_avgGrades['Student Name'] = student_grades['Student Name']
+student_avgGrades['Average Marks'] = student_grades.iloc[:, 2:].mean(axis=1)
+
+# Writing Average Marks to a new CSV file
+student_avgGrades.to_csv('student_average_grades.csv', index=False)
