7c7264313217d000680319c8cff23822b22d6ccd
Uday Valera <126170806+udayvalera@users.noreply.github.com>
2024-08-27 16:42:49 +0000

    Pract-4: Added json files for countries and covid19_summary.json

--- Pract-4/jsonHandling.py
+++ Pract-4/jsonHandling.py
@@ -1,7 +1,70 @@
 import os
 import json
 
-getcwd = os.getcwd()
-listdir = os.listdir(getcwd)
+
+def readJson(datadir):
+    countries = []
+    for root, _, files in os.walk(getcwd):
+        for file in files:
+            if file.endswith('.json') and file != 'covid19_summary.json':
+                with open(os.path.join(root, file), 'r') as f:
+                    data = json.load(f)
+                    countries.append(data)
+    return countries
+
+def statisticsJson(countries):
+    country_stats = []
+    for country in countries:
+        # print(country['country'])
+        # print(country['data'][0]['date'])
+        # print(country['data'][0]['confirmed_cases']['total'])
+        # print(country['data'][0]['deaths']['total'])
+        # print(country['data'][0]['recovered']['total'])
+        country_name = country['country']
+        avg_confirmed_cases = 0
+        avg_deaths = 0
+        avg_recovered = 0
+        max_cases = 0
+        for i in range(0, len(country['data'])):
+            avg_confirmed_cases += country['data'][i]['confirmed_cases']['total']
+            avg_deaths += country['data'][i]['deaths']['total']
+            avg_recovered += country['data'][i]['recovered']['total']
+            if country['data'][i]['confirmed_cases']['total'] > max_cases:
+                max_cases = country['data'][i]['confirmed_cases']['total']
+                max_cases_date = country['data'][i]['date']
+
+        avg_confirmed_cases = avg_confirmed_cases / len(country['data'])
+        avg_deaths = avg_deaths / len(country['data'])
+        avg_recovered = avg_recovered / len(country['data'])
+        
+        
+        country_stats.append({
+            'country': country_name,
+            'confirmed_cases': avg_confirmed_cases,
+            'deaths': avg_deaths,
+            'recovered': avg_recovered,
+            'max_cases': max_cases,
+            'max_date': max_cases_date
+        })
+    return country_stats
+
+def countries_ranking(country_ranking):
+    country_ranking.sort(key=lambda x: x['confirmed_cases'], reverse=True)
+    return country_ranking[:5],country_ranking[-5:]
+
+def summary_report(datadir):
+    countries = readJson(datadir)
+    stats = statisticsJson(countries)
+    top5, bottom5 = countries_ranking(stats)
+
+    #Write the report to a summary json file
+    with open('covid19_summary.json', 'w') as f:
+        json.dump(stats, f, indent=4)
 
 
+
+
+if __name__ == "__main__":
+    getcwd = os.getcwd()
+    datadir = os.path.join(getcwd, "data")
+    summary_report(datadir)
